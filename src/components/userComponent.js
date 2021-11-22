aaab4cb8642ccc5cb1825fb37e7f72da14149d3f
Furkan <furkan.kgz@gmail.com>
2021-11-22 23:39:31 +0300

    22 Kasım Ders Sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger , MongoLogger} from "../crossCuttingConcerns/logging/logger.js";
+import Customer from "../models/customer.js";
 import User from "../models/user.js";
 import UserService from "../services/userService.js"
 
@@ -11,10 +12,20 @@ let userService = new UserService(logger1)
 // userService.getById(1);
 
 
-let user1 = new User(1, "Furkan", "Korkmazgöz", "İstanbul");
-let user2 = new User(2, "Enes", "Korkmazgöz", "İstanbul");
-userService.add(user1);
-userService.add(user2);
+//let user1 = new User(1, "Furkan", "Korkmazgöz", "İstanbul");
+//let user2 = new User(2, "Enes", "Korkmazgöz", "İstanbul");
+//userService.add(user1);
+//userService.add(user2);
 
-console.log(userService.list())
-console.log(userService.getById(2))
+
+console.log("--------------------------")
+userService.Load();
+
+let customerToAdd = new Customer(1, "Seda", "Yılmaz", "Ankara", "aaaa")
+customerToAdd.type = "customer"
+userService.add(customerToAdd)
+
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomersSorted())
---
+++ src/data/users.js
@@ -0,0 +1,48 @@
+export const users = [
+    {
+        id: 1,
+        firstName: "Engin",
+        lastName: "Demiroğ",
+        city: "Ankara",
+        age: 36,
+        creditCardNumber: "12345",
+        type: "customer"
+    },
+    {
+        id: 2,
+        firstName: "Furkan",
+        lastName: "KORKMAZGÖZ",
+        city: "İstanbul",
+        age: 25,
+        creditCardNumber: "123456",
+        type: "customer"
+
+    },
+    {
+        id: 3,
+        firstName: "Ahmet",
+        lastName: "Can",
+        city: "Bursa",
+        age: 22,
+        salary: 25000,
+        type: "employee"
+    },
+    {
+        id: 4,
+        firstName: "Mehmet",
+        lastName: "ÇAKIR",
+        city: "İzmir",
+        age: 22,
+        salary: 5800,
+        type: "employee"
+    },
+    {
+        id: 5,
+        firstName: "Murat",
+        lastName: "Yıldız",
+        city: "Diyarbakır",
+        age: 22,
+        salary: 15800,
+        type: "employee"
+    }
+]
